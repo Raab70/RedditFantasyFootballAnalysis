91b7a077c561c069f1331c9d79d7ea11624e75f7
Raab70 <rob.harrigan89@gmail.com>
2014-09-05 15:20:07 -0500

    download function working well and automatically

--- downloadFP.py
+++ downloadFP.py
@@ -1,26 +1,18 @@
 import os
+#TODO: get weeks to download from the current date
+#TODO: use try/catch to check if weeks have been posted yet.
 
 
 def download_fp(pos, week):
-    #Check how old the player data is
-    download = True
 
-    #if not os.path.isfile(playersfile):
-    #    open(playersfile, 'a').close()
-    #    download = True
-
-    #edit_time = datetime.datetime.fromtimestamp(os.path.getctime(playersfile))
-    #now = datetime.datetime.now()
-    #td = now - edit_time
-    if download:
-        #The file is old, download a new one
-        curl_str = ("curl -s http://www.fantasypros.com/nfl/rankings/%s.php?export=xls > "
-                    "%s/week-%s-%s-raw.xls" % (pos, os.path.join(os.getcwd(), "fp"), week, pos))
-        sed_str = ("sed '1,4d' %s/week-%s-%s-raw.xls > %s/week_%s_%s.tsv"
-                   % (os.path.join(os.getcwd(), "fp"), week, pos, os.path.join(os.getcwd(), "fp"), week, pos))
-        print "Downloading and parsing data for week %d and position %s" % (week, pos)
-        os.system(curl_str)
-        os.system(sed_str)
+    #The file is old, download a new one
+    curl_str = ("curl -s http://www.fantasypros.com/nfl/rankings/%s.php?export=xls > "
+                "%s/week-%s-%s-raw.xls" % (pos, os.path.join(os.getcwd(), "fp"), week, pos))
+    sed_str = ("sed '1,4d' %s/week-%s-%s-raw.xls > %s/week_%s_%s.tsv"
+               % (os.path.join(os.getcwd(), "fp"), week, pos, os.path.join(os.getcwd(), "fp"), week, pos))
+    print "Downloading and parsing FantasyPros data for week %d and position %s" % (week, pos)
+    os.system(curl_str)
+    os.system(sed_str)
 
     #now load the file, it is current
     with open("%s/week_%s_%s.tsv" % (os.path.join(os.getcwd(), "fp"), week, pos), 'r') as tsv:
--- download_fantasy_comments.py
+++ download_fantasy_comments.py
@@ -3,20 +3,24 @@ import praw
 import argparse
 import pickle
 import os
+import datetime
 
 #User defined modules
 from downloadFP import download_fp
-
-#TODO: Compare Reddit suggestions to FP rankings
+from parseReddit import initial_split
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Analyze Reddit FF recommendations')
-    parser.add_argument('--week', type=int, required=True,
-                        help='The current NFL week to be analyzed (REQUIRED)')
+    parser = argparse.ArgumentParser(description='Downloads FF recommendations from Reddit, '
+                                     'as well as the current weeks fantasypros rankings\n'
+                                     'Should be run at least once a week, to not lose fp rankings\n'
+                                     'ideally on saturday to get all reddit comments')
     parser.add_argument('--positions',
                         help='comma separated positions to be analyzed, default is all, valid options are rb,wr,flex',
                         metavar='POS1,POS2,...')
+    parser.add_argument('--fp', help='flag to download fp data, default is true, keep false on sundays\n'
+                                     '(I think, watch output to make sure its saved as the correct week)',
+                        action='store_true', default=True, required=False)
     parser.add_argument('--verbose', help='print verbose outputs',
                         action='store_true', default=False, required=False)
 
@@ -25,15 +29,23 @@ def parse_args():
 
 if __name__ == '__main__':
     #Initial setup bullshit
+    season_start = datetime.datetime(2014, 8, 31, 0, 0)
     options = parse_args()
     verbose = options.verbose
+    dl_fp = options.fp
 
     if options.positions is not None:
         positions = options.positions.split(',')
     else:
         positions = ['wr', 'rb', 'flex']
 
-    week = options.week
+    #Download this weeks fp data:
+    #Rank, Name, team, matchup, low, high, ave, SD
+    days_from_season_start = (datetime.datetime.today() - season_start).days
+    week_now = (days_from_season_start / 7) + 1
+    if dl_fp:
+        for position in positions:
+            all_player_data = download_fp(position, week_now)
 
     #Now scrape reddit
     r = praw.Reddit(user_agent='RCR')
@@ -41,10 +53,8 @@ if __name__ == '__main__':
     print "Analyzing %d positions: %s" % (len(positions), ",".join(positions))
     for position in positions:
         print "Beginning position %s" % position
-        #Rank, Name, team, matchup, low, high, ave, SD
-        all_player_data = download_fp(position, week)
 
-        player_names = [row[1] for row in all_player_data]
+        #player_names = [row[1] for row in all_player_data]
         submissions = subreddit.search('OFFICIAL [WDIS %s] THREAD: ' % (position.upper()),
                                        period='week', sort='new')
 
@@ -54,15 +64,20 @@ if __name__ == '__main__':
         print "Beginning analysis on %d threads" % (len(threads))
 
         for thread in threads:
-
+            if verbose:
+                print "\n"
+                print "Analyzing thread: %s" % (str(thread))
             #Populate the MoreComments objects so we have all comments
             nd = ['placeholder']
             while len(nd) > 0:
                 print("Getting more comments. We currently have %d comments."
                       " (This can take a while) ..." % (len(thread.comments)-1))
                 nd = praw.objects.Submission.replace_more_comments(thread)
-
+            print "Finished getting comments. Final total is: %d" % (len(thread.comments))
             thread_date = str(thread).split(",")[-1].strip().replace("/", "-")
+            thread_date_obj = datetime.datetime.strptime(thread_date, "%m-%d-%Y")
+            days_from_season_start = (thread_date_obj - season_start).days
+            week = (days_from_season_start / 7) + 1
             #DIRECTORY STRUCTURE:
             base_directory = os.path.join(os.getcwd(), "comments")
             week_directory = os.path.join(base_directory, "week_%02d" % week)
@@ -75,7 +90,8 @@ if __name__ == '__main__':
                 os.mkdir(week_directory)
             if not os.path.isdir(position_directory):
                 os.mkdir(position_directory)
-            filename = os.path.join(position_directory, "%s_%s_week-%d.pickle" % (thread_date, position, week))
 
+            filename = os.path.join(position_directory, "%s_%s_week-%d.pickle" % (thread_date, position, week))
+            split_comments = initial_split(thread.comments)
             with open(filename, 'w') as fp:
-                pickle.dump(nd, fp)
+                pickle.dump(split_comments, fp)
